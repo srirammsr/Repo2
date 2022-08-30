ceacf73a3469a3166c6d086eae2c8f8201baf8e1
sri ram murthy <srirammsr@gmail.com>
2022-08-30 15:56:39 +0530

    FirstTime

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,10 +22,124 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtxFolder = New System.Windows.Forms.TextBox()
+        Me.FolderDiag = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.BtnClose = New System.Windows.Forms.Button()
+        Me.btnOpenFolder = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(34, 53)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(60, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "File Name"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(124, 45)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(270, 23)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(34, 18)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(81, 15)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Default Folder"
+        '
+        'txtxFolder
+        '
+        Me.txtxFolder.Location = New System.Drawing.Point(124, 11)
+        Me.txtxFolder.Name = "txtxFolder"
+        Me.txtxFolder.Size = New System.Drawing.Size(270, 23)
+        Me.txtxFolder.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(34, 88)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(50, 15)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Content"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.AcceptsTab = True
+        Me.TextBox3.Location = New System.Drawing.Point(124, 86)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(270, 236)
+        Me.TextBox3.TabIndex = 5
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(69, 338)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(76, 29)
+        Me.btnSave.TabIndex = 6
+        Me.btnSave.Text = "&Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'BtnClose
+        '
+        Me.BtnClose.Location = New System.Drawing.Point(400, 338)
+        Me.BtnClose.Name = "BtnClose"
+        Me.BtnClose.Size = New System.Drawing.Size(76, 29)
+        Me.BtnClose.TabIndex = 7
+        Me.BtnClose.Text = "&Close"
+        Me.BtnClose.UseVisualStyleBackColor = True
+        '
+        'btnOpenFolder
+        '
+        Me.btnOpenFolder.Location = New System.Drawing.Point(405, 11)
+        Me.btnOpenFolder.Name = "btnOpenFolder"
+        Me.btnOpenFolder.Size = New System.Drawing.Size(30, 20)
+        Me.btnOpenFolder.TabIndex = 8
+        Me.btnOpenFolder.Text = "..."
+        Me.btnOpenFolder.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(576, 375)
+        Me.Controls.Add(Me.btnOpenFolder)
+        Me.Controls.Add(Me.BtnClose)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtxFolder)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtxFolder As TextBox
+    Friend WithEvents FolderDiag As FolderBrowserDialog
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents btnSave As Button
+    Friend WithEvents BtnClose As Button
+    Friend WithEvents btnOpenFolder As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,7 @@
 ﻿Public Class Form1
-
+    Private Sub btnOpenFolder_Click(sender As Object, e As EventArgs) Handles btnOpenFolder.Click
+        If (FolderDiag.ShowDialog() = DialogResult.OK) Then
+            txtxFolder.Text = FolderDiag.SelectedPath
+        End If
+    End Sub
 End Class
