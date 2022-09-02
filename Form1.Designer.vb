8de97e18cf4b20f0b1ee09ba84c93e5beef6c517
sri ram murthy <srirammsr@gmail.com>
2022-09-02 12:33:09 +0530

    Done with All changes

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class Form1
+Partial Class FrmSavetoTextFile
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -23,37 +23,39 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txtFileName = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.txtxFolder = New System.Windows.Forms.TextBox()
         Me.FolderDiag = New System.Windows.Forms.FolderBrowserDialog()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.txtContent = New System.Windows.Forms.TextBox()
         Me.btnSave = New System.Windows.Forms.Button()
         Me.BtnClose = New System.Windows.Forms.Button()
         Me.btnOpenFolder = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(34, 53)
+        Me.Label1.Location = New System.Drawing.Point(101, 56)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(60, 15)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "File Name"
         '
-        'TextBox1
+        'txtFileName
         '
-        Me.TextBox1.Location = New System.Drawing.Point(124, 45)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(270, 23)
-        Me.TextBox1.TabIndex = 1
+        Me.txtFileName.Location = New System.Drawing.Point(191, 48)
+        Me.txtFileName.Name = "txtFileName"
+        Me.txtFileName.Size = New System.Drawing.Size(369, 23)
+        Me.txtFileName.TabIndex = 1
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(34, 18)
+        Me.Label2.Location = New System.Drawing.Point(101, 21)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(81, 15)
         Me.Label2.TabIndex = 2
@@ -61,71 +63,89 @@ Partial Class Form1
         '
         'txtxFolder
         '
-        Me.txtxFolder.Location = New System.Drawing.Point(124, 11)
+        Me.txtxFolder.Location = New System.Drawing.Point(191, 14)
         Me.txtxFolder.Name = "txtxFolder"
-        Me.txtxFolder.Size = New System.Drawing.Size(270, 23)
-        Me.txtxFolder.TabIndex = 3
+        Me.txtxFolder.Size = New System.Drawing.Size(369, 23)
+        Me.txtxFolder.TabIndex = 1
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(34, 88)
+        Me.Label3.Location = New System.Drawing.Point(101, 91)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(50, 15)
         Me.Label3.TabIndex = 4
         Me.Label3.Text = "Content"
         '
-        'TextBox3
+        'txtContent
         '
-        Me.TextBox3.AcceptsTab = True
-        Me.TextBox3.Location = New System.Drawing.Point(124, 86)
-        Me.TextBox3.Multiline = True
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(270, 236)
-        Me.TextBox3.TabIndex = 5
+        Me.txtContent.AcceptsTab = True
+        Me.txtContent.Location = New System.Drawing.Point(191, 89)
+        Me.txtContent.Multiline = True
+        Me.txtContent.Name = "txtContent"
+        Me.txtContent.Size = New System.Drawing.Size(416, 236)
+        Me.txtContent.TabIndex = 2
         '
         'btnSave
         '
-        Me.btnSave.Location = New System.Drawing.Point(69, 338)
+        Me.btnSave.Location = New System.Drawing.Point(191, 337)
         Me.btnSave.Name = "btnSave"
         Me.btnSave.Size = New System.Drawing.Size(76, 29)
-        Me.btnSave.TabIndex = 6
+        Me.btnSave.TabIndex = 3
         Me.btnSave.Text = "&Save"
         Me.btnSave.UseVisualStyleBackColor = True
         '
         'BtnClose
         '
-        Me.BtnClose.Location = New System.Drawing.Point(400, 338)
+        Me.BtnClose.Location = New System.Drawing.Point(531, 337)
         Me.BtnClose.Name = "BtnClose"
         Me.BtnClose.Size = New System.Drawing.Size(76, 29)
-        Me.BtnClose.TabIndex = 7
+        Me.BtnClose.TabIndex = 4
         Me.BtnClose.Text = "&Close"
         Me.BtnClose.UseVisualStyleBackColor = True
         '
         'btnOpenFolder
         '
-        Me.btnOpenFolder.Location = New System.Drawing.Point(405, 11)
+        Me.btnOpenFolder.Location = New System.Drawing.Point(577, 14)
         Me.btnOpenFolder.Name = "btnOpenFolder"
         Me.btnOpenFolder.Size = New System.Drawing.Size(30, 20)
-        Me.btnOpenFolder.TabIndex = 8
+        Me.btnOpenFolder.TabIndex = 0
         Me.btnOpenFolder.Text = "..."
         Me.btnOpenFolder.UseVisualStyleBackColor = True
         '
-        'Form1
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(4, 4)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(41, 15)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Label4"
+        '
+        'Panel1
+        '
+        Me.Panel1.Location = New System.Drawing.Point(3, 26)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(63, 397)
+        Me.Panel1.TabIndex = 6
+        '
+        'FrmSavetoTextFile
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(576, 375)
+        Me.ClientSize = New System.Drawing.Size(634, 427)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.btnOpenFolder)
         Me.Controls.Add(Me.BtnClose)
         Me.Controls.Add(Me.btnSave)
-        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.txtContent)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.txtxFolder)
         Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.txtFileName)
         Me.Controls.Add(Me.Label1)
-        Me.Name = "Form1"
+        Me.Name = "FrmSavetoTextFile"
         Me.Text = "Save to text file"
         Me.ResumeLayout(False)
         Me.PerformLayout()
@@ -133,13 +153,15 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents Label1 As Label
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents txtFileName As TextBox
     Friend WithEvents Label2 As Label
     Friend WithEvents txtxFolder As TextBox
     Friend WithEvents FolderDiag As FolderBrowserDialog
     Friend WithEvents Label3 As Label
-    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents txtContent As TextBox
     Friend WithEvents btnSave As Button
     Friend WithEvents BtnClose As Button
     Friend WithEvents btnOpenFolder As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Panel1 As Panel
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,7 +1,24 @@
-﻿Public Class Form1
+﻿Public Class FrmSavetoTextFile
     Private Sub btnOpenFolder_Click(sender As Object, e As EventArgs) Handles btnOpenFolder.Click
         If (FolderDiag.ShowDialog() = DialogResult.OK) Then
             txtxFolder.Text = FolderDiag.SelectedPath
         End If
     End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        Dim FILE_NAME As String = txtxFolder.Text & "\ " & Trim(txtFileName.Text) & ".txt"
+startwriting:
+        If System.IO.File.Exists(FILE_NAME) = True Then
+            My.Computer.FileSystem.WriteAllText(FILE_NAME, txtContent.Text & vbCrLf, True)
+            MsgBox("Content saved", MsgBoxStyle.OkOnly + vbInformation, "File Save")
+        Else
+            System.IO.File.Create(FILE_NAME).Dispose()
+            GoTo startwriting
+        End If
+    End Sub
+
+    Private Sub BtnClose_Click(sender As Object, e As EventArgs) Handles BtnClose.Click
+        Me.Close()
+    End Sub
 End Class
+
